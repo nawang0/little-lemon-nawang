33210e4b019052a9847dd69d4b878eb65bdc637a
NTENDAR <nawang.tendar@capgemini.com>
2024-10-24 11:44:29 +0200

    cleanup code

--- src/views/BookingPage.js
+++ src/views/BookingPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import BookingForm from '../components/BookingForm'
 import Hero from '../components/heroBanner/Hero'
-import heroImage from '../assets/images/restauranfood.jpg'
+
 
 export default function BookingPage() {
 
